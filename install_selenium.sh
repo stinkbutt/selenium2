c133ace33f5d5d125c195746566b2c34e0def75f
stinkbutt <10701211+stinkbutt@users.noreply.github.com>
2018-10-30 12:08:08 -0400

    Switched to OpenJDK

--- install_selenium.sh
+++ install_selenium.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
 # Used to install java, google chrome stable, chromedriver and Selenium Standalone
-sudo add-apt-repository main
-sudo add-apt-repository universe
+sudo add-apt-repository universe # for xvfb
 
-sudo add-apt-repository ppa:webupd8team/java
+# sudo add-apt-repository ppa:webupd8team/java 		# Installing openjdk via apt instead
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
 # Install prerequisites
 sudo apt-get update
-sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4 oracle-java8-installer google-chrome-stable
+sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4 oracle-java8-installer google-chrome-stable firefox openjdk-8-jdk
 
 #mkdir ./temp && cd ./temp
 
-# Install Chrome Driver
 #wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
 #wget https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar
 #wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 unzip chromedriver_linux64.zip
-tar xvfb geckodriver-v0.23.0-linux64.tar.gz
+tar zxfv geckodriver-v0.23.0-linux64.tar.gz
 
 sudo chown root:root *
 
--- start_selenium.sh
+++ start_selenium.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -Dwebdriver.chrome.driver="/usr/bin/chromedriver" -Dwebdriver.gecko.driver="/usr/bin/geckodriver" -jar /usr/bin/selenium-server-standalone-3.13.0.jar
+svfb-run -a java -Dwebdriver.chrome.driver="/usr/bin/chromedriver" -Dwebdriver.gecko.driver="/usr/bin/geckodriver" -jar /usr/bin/selenium-server-standalone-3.13.0.jar
