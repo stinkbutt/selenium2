c0df6a366f88ce1fc1b912e1d02fd1f5970e197c
stinkbutt <10701211+stinkbutt@users.noreply.github.com>
2018-10-30 11:26:37 -0400

    Added binaries

--- install_selenium.sh
+++ install_selenium.sh
@@ -1,5 +1,6 @@
-# Used to install java, google chrome stable, chromedriver and Selenium Standalone
+#!/bin/bash
 
+# Used to install java, google chrome stable, chromedriver and Selenium Standalone
 sudo add-apt-repository main
 sudo add-apt-repository universe
 
@@ -11,16 +12,18 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 sudo apt-get update
 sudo apt-get install -y unzip xvfb libxi6 libgconf-2-4 oracle-java8-installer google-chrome-stable
 
-mkdir ./temp && cd ./temp
+#mkdir ./temp && cd ./temp
 
 # Install Chrome Driver
-wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
-wget https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar
-wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
+#wget https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip
+#wget https://selenium-release.storage.googleapis.com/3.13/selenium-server-standalone-3.13.0.jar
+#wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 unzip chromedriver_linux64.zip
 tar xvfb geckodriver-v0.23.0-linux64.tar.gz
 
 sudo chown root:root *
+
+sudo mv selenium-server-standalone-3.13.0.jar /usr/bin/selenium-server-standalone-3.13.0.jar
 sudo mv chromedriver /usr/bin/chromedriver
 sudo mv geckodriver /usr/bin/geckodriver
 sudo chmod +x /usr/bin/chromedriver /usr/bin/geckodriver
